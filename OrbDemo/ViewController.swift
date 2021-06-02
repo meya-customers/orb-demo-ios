1960f9ade969e479768a65ee05b6d423b4d077f7
Roeland van Nieuwkerk <roeland@meya.ai>
2021-06-01 20:05:08 -0400

    Initial commit

---
+++ OrbDemo/ViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+import Flutter
+import orb
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.addTarget(self, action: #selector(showOrb), for: .touchUpInside)
+        button.setTitle("Show Orb", for: UIControl.State.normal)
+        button.frame = CGRect(x: 80.0, y: 210.0, width: 160.0, height: 40.0)
+        button.backgroundColor = UIColor.blue
+        self.view.addSubview(button)
+        
+    }
+    
+    @IBAction func showOrb(sender: UIButton) {
+        let orb = (UIApplication.shared.delegate as! AppDelegate).orb
+        let platformVersion = "iOS " + UIDevice.current.systemVersion
+        orb.connect(
+            options: OrbConnectionOptions(
+                gridUrl: "https://grid-rvn-dev.meya.ai",
+                appId: "app-edf4be8b0f984a8db8823d8074beeb83",
+                integrationId: "integration.orb",
+                pageContext: [
+                    "platform_version": platformVersion,
+                    "a": 1234,
+                    "data": [
+                        "key1": "value1",
+                        "key2": 123123.3,
+                        "bool": true
+                    ]
+                ] as [String: Any?]
+            ),
+            result: { result in
+                print("Connect result: \(String(describing: result))")
+            })
+        let viewController = orb.viewController()
+        present(viewController, animated: true, completion: nil)
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+flutter_application_path = '../v2/orb/mobile/orb_sdk'
+load File.join(flutter_application_path, '.ios', 'Flutter', 'podhelper.rb')
+
+platform :ios, '9.0'
+
+target 'OrbDemo' do
+    use_frameworks!
+    install_all_flutter_pods(flutter_application_path)
+end
