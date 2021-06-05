beef707ef5d99938042a32e7dfa3848557fc2b98
Roeland van Nieuwkerk <roeland@meya.ai>
2021-06-05 09:13:17 -0400

    Open orb in modal mode and page view mode. Added icons, styling and orb settings form.

---
+++ OrbDemo/ChatViewController.swift
@@ -0,0 +1,68 @@
+import UIKit
+import orb
+
+class ChatViewController: UIViewController {
+    
+    var gridUrl: String = "https://grid.meya.ai"
+    var appId: String = "app-73c6d31d4f544a72941e21fb518b5737"
+    var integrationId: String = "integration.orb"
+    var pageContext: [String: Any?] = [:]
+    
+    @IBOutlet weak var chatView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Orb"
+        createOrbViewController()
+    }
+
+    func createOrbViewController() {
+        let orb = (UIApplication.shared.delegate as! AppDelegate).orb
+        orb.connect(
+            options: OrbConnectionOptions(
+                gridUrl: gridUrl,
+                appId: appId,
+                integrationId: integrationId,
+                pageContext: pageContext
+            ),
+            result: { result in
+                print("Connect result: \(String(describing: result))")
+            })
+        orb.onConnnected {
+            print("Orb connected")
+        }
+        orb.onDisconnected {
+            print("Orb disconnected")
+        }
+        let orbViewController = orb.viewController()
+        
+        addChild(orbViewController)
+        orbViewController.didMove(toParent: self)
+        orbViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let views: [String: Any] = ["orbView": orbViewController.view!]
+        
+        view.addSubview(orbViewController.view)
+        view.addConstraints(
+            NSLayoutConstraint.constraints(
+                withVisualFormat: "H:|-0-[orbView]-0-|",
+                options: NSLayoutConstraint.FormatOptions(rawValue: 0),
+                metrics: nil,
+                views: views
+            )
+        )
+        view.addConstraints(
+            NSLayoutConstraint.constraints(
+                withVisualFormat: "V:|-0-[orbView]-0-|",
+                options: NSLayoutConstraint.FormatOptions(rawValue: 0),
+                metrics: nil,
+                views: views
+            )
+        )
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        let orb = (UIApplication.shared.delegate as! AppDelegate).orb
+        orb.disconnect()
+    }
+}
---
+++ OrbDemo/LaunchViewController.swift
@@ -0,0 +1,84 @@
+import UIKit
+import Flutter
+import orb
+
+class LaunchViewController: UIViewController {
+    @IBOutlet weak var gridUrlField: UITextField!
+    @IBOutlet weak var appIdField: UITextField!
+    @IBOutlet weak var integrationIdField: UITextField!
+    @IBOutlet weak var logoView: UIImageView!
+    @IBOutlet weak var launchOrbButton: UIButton!
+    @IBOutlet weak var launchModalButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Orb Demo"
+        
+        gridUrlField.delegate = self
+        appIdField.delegate = self
+        integrationIdField.delegate = self
+        
+        logoView.image = UIImage(named: "Logo")
+        
+        launchOrbButton.layer.cornerRadius = 8
+        launchOrbButton.layer.masksToBounds = true
+        
+        launchModalButton.layer.cornerRadius = 8
+        launchModalButton.layer.masksToBounds = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.destination is ChatViewController {
+            let chatViewController = segue.destination as? ChatViewController
+            let platformVersion = "iOS " + UIDevice.current.systemVersion
+            chatViewController?.gridUrl = gridUrlField.text!
+            chatViewController?.appId = appIdField.text!
+            chatViewController?.integrationId = integrationIdField.text!
+            chatViewController?.pageContext = [
+                "platform_version": platformVersion,
+                "a": 1234,
+                "data": [
+                    "key1": "value1",
+                    "key2": 123123.3,
+                    "bool": true
+                ]
+            ] as [String: Any?]
+        }
+    }
+    
+    @IBAction func launchOrbModal(_ sender: Any) {
+        let orb = (UIApplication.shared.delegate as! AppDelegate).orb
+        let platformVersion = "iOS " + UIDevice.current.systemVersion
+        
+        orb.connect(
+            options: OrbConnectionOptions(
+                gridUrl: gridUrlField.text!,
+                appId: appIdField.text!,
+                integrationId: integrationIdField.text!,
+                pageContext: [
+                    "platform_version": platformVersion,
+                    "a": 1234,
+                    "data": [
+                        "key1": "value1",
+                        "key2": 123123.3,
+                        "bool": true
+                    ]
+                ] as [String: Any?]
+            ),
+            result: { result in
+                print("Connect result: \(String(describing: result))")
+            })
+        let orbViewController = orb.viewController()
+        orbViewController.modalPresentationStyle = .fullScreen
+        orbViewController.modalTransitionStyle = .crossDissolve
+        present(orbViewController, animated: true, completion: nil)
+    }
+}
+
+extension LaunchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
